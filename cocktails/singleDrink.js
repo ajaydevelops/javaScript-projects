acf2a10f8fe05bde18b129d22b1fa5c279e9d903
Ajay Singh <ajaydevelop01@gmail.com>
2022-03-26 07:39:11 +0530

    Adding cocotails.html

---
+++ cocktails/singleDrink.js
@@ -0,0 +1,32 @@
+// import getCocktails from "./src/fetchDrinks";
+
+// let drinks;
+
+let mainView = document.getElementById("mainView");
+
+// getCocktails()
+//   .then((data) => {
+//     drinks = data;
+//     getSingleDrink();
+//   })
+//   .catch((error) => console.log(error));
+
+let getSingleDrink = () => {
+  let a = localStorage.getItem("seleDrin");
+  let b = JSON.parse(a);
+  let singleDrink = b[0];
+
+  let div = document.createElement("div");
+  div.classList.add("drink");
+  let img = document.createElement("img");
+  img.src = `${singleDrink.strDrinkThumb}`;
+  img.classList.add("img");
+  let para = document.createElement("p");
+  para.textContent = `${singleDrink.strDrink}`;
+  para.classList.add("name");
+  div.appendChild(img);
+  div.appendChild(para);
+  mainView.appendChild(div);
+};
+
+getSingleDrink();
