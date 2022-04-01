640720391f7c649ee10778114424d79fd5b848f3
Ajay Singh <ajaydevelop01@gmail.com>
2022-04-01 15:24:33 +0530

    WIP

--- randomUser/app.js
+++ randomUser/app.js
@@ -19,6 +19,7 @@ let getRandomUser = async () => {
   const response = await fetch(url);
   let data = await response.json();
   persons = data.results;
+  perInfoContent = `${persons[0].name.first} ${persons[0].name.last}`;
   getPerson();
 };
 
@@ -26,33 +27,35 @@ getRandomUser();
 
 
 let getPerson = function () {
-  perInfoContent = `${persons[0].name.title} ${persons[0].name.first} ${persons[0].name.last}`;
   avatar.src = persons[0].picture.large;
   label.textContent = lblContent;
   persInfo.textContent = perInfoContent;
 };
 
 btnBox.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.target.dataset.id);
   let icons = document.getElementsByTagName("i");
   let arr = Array.from(icons);
   let iconId = arr.filter(
     (element) => element.dataset.id === e.target.dataset.id
   );
-  let id = Number(iconId[0].dataset.id);
+  let id = iconId[0].dataset.id;
+  id = Number(id);
   switch (id) {
     case 1:
       lblContent = "My name is";
-      perInfoContent = `${persons[0].name.title} ${persons[0].name.first} ${persons[0].name.last}`;
+      perInfoContent = `${persons[0].name.first} ${persons[0].name.last}`;
       break;
 
     case 2:
       lblContent = "My email is";
-      perInfoContent = persons[0].email;
+      perInfoContent = `${persons[0].email}`;
       break;
 
     case 3:
       lblContent  = "My age is";
-      perInfoContent = persons[0].dob.age;
+      perInfoContent = `${persons[0].dob.age}`;
       break;
 
     case 4:
@@ -62,12 +65,12 @@ btnBox.addEventListener("click", (e) => {
 
     case 5:
       lblContent = "My phone is";
-      perInfoContent = persons[0].phone;
+      perInfoContent = `${persons[0].phone}`;
       break;
 
     case 6:
       lblContent = "My password is";
-      perInfoContent = persons[0].login.password;
+      perInfoContent = `${persons[0].login.password}`;
       break;
 
     default:
